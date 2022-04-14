7eee5c182d0cc9a5fd4ad8bd2ea9a8746bd3be8a
ghasemi.sh <100312857+ghasemi18sh@users.noreply.github.com>
2022-04-14 17:23:55 +0430

    Add files via upload

---
+++ safhe shatranji.py
@@ -0,0 +1,11 @@
+def CheSs(N,M):
+    for i in range(int(N)):
+        for j in range(int(M)):
+            if (i + j) % 2 == 0:
+                print("🟨",end="")
+            elif (i + j) % 2 != 0:
+                print("🟩",end="")
+        print("\n",end="")
+N = (input("Enter N :"))
+M = (input("Enter M :"))
+CheSs(int(N),int(M))
